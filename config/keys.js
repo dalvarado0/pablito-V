cc56fc36c1c46c5be8b27b97528d830ce203a4cf
David Alejandro Alvarado Villa <david.alvarado.v@hotmail.com>
2018-10-26 17:16:06 -0500

    first commit

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports =
+{
+	googleClientID: '37375176951-8q98t5v0ha79echt6acentm6lu404ufi.apps.googleusercontent.com',
+	googleClientSecret: '4QMDyGrbn9bmHCJhjIyiJEnx',
+	mongoURI: 'mongodb://dalvarado0:mypass12@ds149252.mlab.com:49252/mydb-mongo',
+	cookiekey: 'nasinckjasndsadkjccmnweoiocxivqw'
+};
